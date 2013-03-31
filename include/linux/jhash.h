3ec90394af39fa8f9132c62070e20739ea79b3e4
TheGreatsega <jsehgal.92@gmail.com>
2013-03-31 18:45:37 +0530

    BFQ Sched

--- include/linux/jhash.h
+++ include/linux/jhash.h
@@ -12,32 +12,29 @@
  * lookup3.c, by Bob Jenkins, May 2006, Public Domain.
  *
  * These are functions for producing 32-bit hashes for hash table lookup.
- * hashword(), hashlittle(), hashlittle2(), hashbig(), mix(), and final()
- * are externally useful functions. Routines to test the hash are included
- * if SELF_TEST is defined. You can use this free for any purpose. It's in
- * the public domain. It has no warranty. 
- * 
+ * hashword(), hashlittle(), hashlittle2(), hashbig(), mix(), and final() 
+ * are externally useful functions.  Routines to test the hash are included 
+ * if SELF_TEST is defined.  You can use this free for any purpose.  It's in
+ * the public domain.  It has no warranty.
+ *
  * Copyright (C) 2009 Jozsef Kadlecsik (kadlec@blackhole.kfki.hu)
  *
  * I've modified Bob's hash to be useful in the Linux kernel, and
- * any bugs present are my fault. Jozsef
+ * any bugs present are my fault.  Jozsef
  */
 
 #define __rot(x,k) (((x)<<(k)) | ((x)>>(32-(k))))
- 
+
 /* __jhash_mix - mix 3 32-bit values reversibly. */
 #define __jhash_mix(a,b,c) \
 { \
-  a -= c; a ^= __rot(c, 4); c += b; \
-  b -= a; b ^= __rot(a, 6); a += c; \
-  c -= b; c ^= __rot(b, 8); b += a; \
-  a -= c; a ^= __rot(c,16); c += b; \
-  b -= a; b ^= __rot(a,19); a += c; \
-  c -= b; c ^= __rot(b, 4); b += a; \
+  a -= c;  a ^= __rot(c, 4);  c += b; \
+  b -= a;  b ^= __rot(a, 6);  a += c; \
+  c -= b;  c ^= __rot(b, 8);  b += a; \
+  a -= c;  a ^= __rot(c,16);  c += b; \
+  b -= a;  b ^= __rot(a,19);  a += c; \
+  c -= b;  c ^= __rot(b, 4);  b += a; \
 }
-
-/* The golden ration: an arbitrary value */
-#define JHASH_GOLDEN_RATIO  0x9e3779b9
 
 /* __jhash_final - final mixing of 3 32-bit values (a,b,c) into c */
 #define __jhash_final(a,b,c) \
@@ -46,13 +43,13 @@
   a ^= c; a -= __rot(c,11); \
   b ^= a; b -= __rot(a,25); \
   c ^= b; c -= __rot(b,16); \
-  a ^= c; a -= __rot(c,4); \
+  a ^= c; a -= __rot(c,4);  \
   b ^= a; b -= __rot(a,14); \
   c ^= b; c -= __rot(b,24); \
 }
 
-/* The golden ration: an arbitrary value */
-#define JHASH_GOLDEN_RATIO 0xdeadbeef
+/* An arbitrary initial parameter */
+#define JHASH_GOLDEN_RATIO	0xdeadbeef
 
 /* The most generic version, hashes an arbitrary sequence
  * of bytes.  No alignment or length assumptions are made about
@@ -68,10 +65,10 @@ static inline u32 jhash(const void *key, u32 length, u32 initval)
 
 	/* all but the last block: affect some 32 bits of (a,b,c) */
 	while (length > 12) {
-		a += (k[0] + ((u32)k[1]<<8) + ((u32)k[2]<<16) + ((u32)k[3]<<24));
+    		a += (k[0] + ((u32)k[1]<<8) + ((u32)k[2]<<16) + ((u32)k[3]<<24));
 		b += (k[4] + ((u32)k[5]<<8) + ((u32)k[6]<<16) + ((u32)k[7]<<24));
 		c += (k[8] + ((u32)k[9]<<8) + ((u32)k[10]<<16) + ((u32)k[11]<<24));
-		__jhash_mix(a, b, c); 
+		__jhash_mix(a, b, c);
 		length -= 12;
 		k += 12;
 	}
@@ -93,7 +90,7 @@ static inline u32 jhash(const void *key, u32 length, u32 initval)
 	case 1 : a += k[0];
 		__jhash_final(a, b, c);
 	case 0 :
-	break;
+		break;
 	}
 
 	return c;
@@ -120,19 +117,18 @@ static inline u32 jhash2(const u32 *k, u32 length, u32 initval)
 	}
 
 	/* handle the last 3 u32's */
-	/* all the case statements fall through */
+	/* all the case statements fall through */ 
 	switch (length) {
 	case 3: c += k[2];
 	case 2: b += k[1];
 	case 1: a += k[0];
-	__jhash_final(a, b, c);
-	case 0: /* case 0: nothing left to add */
-	break;
+		__jhash_final(a, b, c);
+	case 0:     /* case 0: nothing left to add */
+		break;
 	}
 
 	return c;
 }
-
 
 /* A special ultra-optimized versions that knows they are hashing exactly
  * 3, 2 or 1 word(s).
